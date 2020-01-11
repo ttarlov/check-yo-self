1116388b3f7d9d8e04b131b1b8407c408282ea8d
Taras Tarlov <ttarlov@gmail.com>
2020-01-11 16:41:49 -0700

    Add query selector for add task button

--- main.js
+++ main.js
@@ -3,29 +3,33 @@ var taskTitleInput = document.querySelector('.title-input-js');
 var taskItemInput = document.querySelector('.task-input-js');
 var unsavedTasks = document.querySelector('.unsaved-tasks');
 var potentialToDo = new ToDoList();
+var taskContainer = document.querySelector('.task-card-container');
+var addTaskBtn = document.querySelector('.add-task-btn-js');
 
 globalButtonEventListener.addEventListener('click', globalButtonEventHandler);
 taskItemInput.addEventListener('click', clearInput);
 taskTitleInput.addEventListener('click', clearInput);
+taskItemInput.addEventListener('input', checkInputForContent);
 
 function globalButtonEventHandler(event) {
   if (event.target.classList.contains('add-task-btn-js')) {
     console.log('add task button pressed');
-    displayUnsavedTaskes();
+    displayUnsavedTasks();
   } else if (event.target.classList.contains('clear-btn-js')) {
     console.log('clear button pressed');
   } else if (event.target.classList.contains('filter-btn-js')) {
     console.log('filter button pressed');
   } else if (event.target.classList.contains('make-task-btn-js')) {
     console.log('make task button pressed', event.target);
+    incertTaskCard();
   } else if (event.target.classList.contains('delete-img')) {
     deleteUnsavedTask(event);
     // console.log(event.target.classList);
   }
 }
 
-function displayUnsavedTaskes() {
-  // debugger
+function displayUnsavedTasks() {
+  addTaskBtn.disabled = true;
   var task = new Task(taskItemInput.value);
   unsavedTasks.insertAdjacentHTML('afterbegin',
   `<div class="saved-task-img">
@@ -49,6 +53,12 @@ function clearInput() {
   }
 }
 
+function checkInputForContent() {
+  if (taskItemInput.value !== '' || 'Add Another Task Name') {
+    addTaskBtn.disabled = false;
+  }
+}
+
 function deleteUnsavedTask(event) {
   let taskToRemove = event.target.dataset.id;
   potentialToDo.removeTask(taskToRemove);
@@ -56,5 +66,33 @@ function deleteUnsavedTask(event) {
 }
 
 function incertTaskCard() {
-  
+  taskContainer.insertAdjacentHTML('afterbegin', `<div class="task-card">
+    <h2> ${taskTitleInput.value}</h2>
+    <div class="saved-tasks-container">
+      <div class="saved-tasks">
+        <div class="saved-task-img">
+          <img class="delete-img" src="assets/checkbox.svg" alt="checkbox"></img>
+          <span class="single-task">Don't ever play yourself.</span>
+        </div>
+        <div class="saved-task-img">
+          <img class="delete-img" src="assets/checkbox.svg" alt="checkbox"></img>
+          <span class="single-task">Don't ever play yourself.</span>
+        </div>
+        <div class="saved-task-img">
+          <img class="delete-img" src="assets/checkbox.svg" alt="checkbox"></img>
+          <span class="single-task">Don't ever play yourself.</span>
+        </div>
+      </div>
+    </div>
+    <div class="urgency-delete-container">
+      <div class="urgent-img-tag">
+        <img src="assets/urgent.svg" alt="urgency level">
+        <p>urgent</p>
+      </div>
+      <div class="delete-img-tag">
+        <img src="assets/delete.svg" alt="delete card button">
+        <p>delete</p>
+      </div>
+    </div>
+  </div>`);
 }
