7bfd9c11555735a36b8d87d18ef464ec2c9ff41d
Taras Tarlov <ttarlov@gmail.com>
2020-01-11 15:52:23 -0700

    add removeTask method

--- todo-list.js
+++ todo-list.js
@@ -10,4 +10,12 @@ class ToDoList {
     this.tasks.push(task);
   }
 
+  removeTask(taskToRemove) {
+    var tasksArr = this.tasks;
+    var actualTaskToRemove = tasksArr.find(task => task.id == taskToRemove);
+    // console.log('actual tasks to remove', actualTaskToRemove);
+    var indexToRemove = tasksArr.indexOf(actualTaskToRemove);
+    tasksArr.splice(indexToRemove, 1);
+    console.log(this.tasks);
+  }
 }
