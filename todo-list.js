d64f68ff9f449bd424ca41f872a82ec96b7fba2c
Taras Tarlov <ttarlov@gmail.com>
2020-01-12 13:09:05 -0700

    Remove console log

--- todo-list.js
+++ todo-list.js
@@ -13,7 +13,6 @@ class ToDoList {
   removeTask(taskToRemove) {
     var tasksArr = this.tasks;
     var actualTaskToRemove = tasksArr.find(task => task.id == taskToRemove);
-    // console.log('actual tasks to remove', actualTaskToRemove);
     var indexToRemove = tasksArr.indexOf(actualTaskToRemove);
     tasksArr.splice(indexToRemove, 1);
     console.log(this.tasks);
